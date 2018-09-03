d7f7b3f17d1f91558bfca640d0aaa16390d8ab74
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-03 17:25:21 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,8 +1,15 @@
 def hello_t(names)
 if block_given?
-  names.each {|x| puts x }
+  names.each {|x| yield(x) }
+  names
 else
  puts "Hey! No block was given!"
 end
 
+
 # call your method here!
+["Tim", "Tom", "Jim"].each do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
